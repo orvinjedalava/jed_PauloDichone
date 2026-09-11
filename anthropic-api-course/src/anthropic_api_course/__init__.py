6d5da2fe26c05a3afedb30f6beed980a3d3092ef
Orvin Jed Alava <orvinjedalava@Orvins-Mac-mini.local>
2026-09-12 00:19:23 +1000

    Adding Memory for multi-turn conversation

--- anthropic-api-course/src/anthropic_api_course/__init__.py
+++ anthropic-api-course/src/anthropic_api_course/__init__.py
@@ -3,20 +3,47 @@ load_dotenv()
 
 # create an API client
 from anthropic import Anthropic
-from anthropic.types import Message
+from anthropic.types import Message, MessageParam
+from typing import Iterable
+
+def add_user_message(messages: list[MessageParam], content: str):
+    add_message(messages, content, "user")
+
+def add_assistant_message(messages: list[MessageParam], content: str):
+    add_message(messages, content, "assistant")
+
+def add_message(messages: list[MessageParam], content: str, role: str):
+    new_message: MessageParam = {
+        "role": role,
+        "content": content
+    }
+    messages.append(new_message)
+
+def chat(messages: Iterable[MessageParam])-> str:
+    client = Anthropic()
+    response: Message = client.messages.create(
+        model="claude-haiku-4-5-20251001",
+        max_tokens=1024,
+        messages=messages,
+    )
+    return response.content[0].text    
 
 
 def main() -> None:
-    client = Anthropic()
+    messages = []
+    add_user_message(messages, "Write a short poem about the sea.")
+    print("Messages;;;;;", messages)
 
-    message: Message = client.messages.create(
-        model="claude-haiku-4-5-20251001",
-        max_tokens=1024,
-        messages=[
-            {
-                "role": "user",
-                "content": "Write a short poem about the sea."
-            }
-        ]
-    )
-    print(message.content)
+    response : str = chat(messages)
+    print("Assistant:", response)
+
+    # Take the answer from the assistant and add it to the assistant message
+    add_assistant_message(messages, response)
+
+    # Now, ask a follow-up question
+    add_user_message(messages, "Can you make it ryhme?")
+    print("Messages;;;;;", messages)
+
+    # call the chat function again with the updated messages
+    response : str = chat(messages)
+    print("Assistant:", response)
